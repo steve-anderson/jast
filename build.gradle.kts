f1977d37ec27bb7477069e982422f2a2a20b46af
Steven Anderson <steve@swahome.net>
2018-08-26 18:57:14 -0500

    clean up gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,8 @@ subprojects {
 
     val serverExtraDeployment by configurations.creating
 
+    val remoteClientCompile by configurations.creating
+
     dependencies {
         add(baseCompile.name, "org.slf4j:slf4j-api:1.7.25")
 
@@ -69,5 +71,12 @@ subprojects {
         add(serverExtraDeployment.name, "org.apache.activemq:activemq-rar:5.15.4") {
             setTransitive(false)
         }
+
+        add(remoteClientCompile.name, "org.jboss.spec.javax.ejb:jboss-ejb-api_3.2_spec:1.0.0.Final")
+        add(remoteClientCompile.name, "org.jboss:jboss-remote-naming:2.0.4.Final")
+        add(remoteClientCompile.name, "org.jboss:jboss-ejb-client:2.1.4.Final")
+        add(remoteClientCompile.name, "org.jboss.xnio:xnio-nio:3.3.4.Final")
+        add(remoteClientCompile.name, "org.jboss.marshalling:jboss-marshalling-river:1.4.10.Final")
+        add(remoteClientCompile.name, "org.jboss.spec.javax.transaction:jboss-transaction-api_1.2_spec:1.0.0.Final")
     }
 }
--- middle/middle-client/build.gradle.kts
+++ middle/middle-client/build.gradle.kts
@@ -6,22 +6,13 @@ plugins {
 base.archivesBaseName = "middle-client"
 
 val serverCompileOnly by configurations
-val compileOnly: Configuration by configurations
+val compileOnly by configurations
 compileOnly.extendsFrom(serverCompileOnly)
+
+val remoteClientCompile by configurations
+val compile by configurations
+compile.extendsFrom(remoteClientCompile)
 
 dependencies {
     compile(project(":middle:middle-api"))
-//    compile ("org.jboss:jboss-remote-naming:2.0.4.Final")
-//    compile("org.wildfly:wildfly-naming-client:1.0.9.Final")
-//    compile("org.wildfly:wildfly-naming:10.1.0.Final")
-//    compile("org.jboss:jboss-ejb-client:2.1.4.Final")
-//    compile("org.jboss.logging:jboss-logging:3.3.0.Final")
-    compile("org.jboss.spec.javax.ejb:jboss-ejb-api_3.2_spec:1.0.0.Final")
-    compile("org.jboss:jboss-remote-naming:2.0.4.Final")
-    compile("org.jboss:jboss-ejb-client:2.1.4.Final")
-    compile("org.jboss.xnio:xnio-nio:3.3.4.Final")
-    compile("org.jboss.marshalling:jboss-marshalling-river:1.4.10.Final")
-    compile("org.jboss.spec.javax.transaction:jboss-transaction-api_1.2_spec:1.0.0.Final")
 }
-
-
