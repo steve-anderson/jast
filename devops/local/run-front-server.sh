decdfc237e79dbc4285b29793d47d139ac1ccf9d
Steven Anderson <steve@swahome.net>
2018-08-26 19:37:01 -0500

    Works on lce.swahome.net!

--- devops/local/run-front-server.sh
+++ devops/local/run-front-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --rm -p 8080:8080 -p 5000:5000 --link middle-server --name front-server dregistry.swahome.net:7443/research/jast/front-server:1.0 /opt/jboss/wildfly/bin/standalone.sh --debug 5000 -b 0.0.0.0
+docker run -d -h front-server -p 8080:8080 -p 5000:5000 --link middle-server --name front-server dregistry.swahome.net:7443/research/jast/front-server:1.0 /opt/jboss/wildfly/bin/standalone.sh --debug 5000 -b 0.0.0.0 -bmanagement 0.0.0.0
